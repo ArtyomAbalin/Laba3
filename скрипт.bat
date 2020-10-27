d1c72890c9d1948919f9c1fb4d0de8cccf77d95e
ArtyomAbalin <inna_artik@mail.ru>
2020-10-27 09:22:18 +0300

    script

---
+++ скрипт.bat
@@ -0,0 +1,26 @@
+echo off
+md Abalin
+cd Abalin
+md Artyom
+cd Artyom
+md Alexandrovich
+cd ..
+pause
+echo> 27101999.txt
+cd Artyom
+cd Alexandrovich
+echo> asus.txt
+cd ..
+cd ..
+cd ..
+pause
+del Abalin /S/Q/F
+pause
+cd Abalin
+cd Artyom
+rd Alexandrovich
+cd ..
+rd Artyom
+cd ..
+rd Abalin
+pause
